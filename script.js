9ef077cc0362cb40c7e976335fb5352087eb5be5
Max Pons <157253032+max1pons@users.noreply.github.com.>
2024-01-22 22:20:22 -0600

    Desafios resueltos Challenges solved

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+let contador = 1;
 
+console.log("1er Desafio: Contador = " + contador);
+
+while (contador < 10) {
+    contador += 1;
+    console.log("Contador = " + contador);
+}
+
+console.log("2do Desafio: Contador = " + contador);
+
+while(contador > 0) {
+    contador -= 1;
+    console.log("Contador = " + contador);
+}
+
+let cuentaProgresiva = prompt("Ecoge un numero y contare hasta llegar a ese numero: ");
+console.log("3er Desafio: Contador = " + contador);
+
+while(contador < cuentaProgresiva) {
+    contador += 1;
+    console.log("Contador = " + contador);
+}
+
+
+let numeroMaximo = prompt("Ingresa un número para la cuenta regresiva:");
+
+while (numeroMaximo >= 0) {
+console.log(numeroMaximo);
+numeroMaximo--;
+}
